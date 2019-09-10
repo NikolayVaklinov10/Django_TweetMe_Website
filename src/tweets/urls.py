2b2ad30d7d1bc6f008298ee9c781a036061c6517
Nick <nikolayvaklinov@yahoo.com>
2019-09-10 19:45:35 +0100

    create view

--- src/tweets/urls.py
+++ src/tweets/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import url
 
 
-from .views import TweetListView, TweetDetailView
+from .views import (
+    TweetCreateView,
+    TweetDetailView,
+    TweetListView
+    )
 
 urlpatterns = [
-    #url(r'^admin/', admin.site.urls),
     url(r'^$', TweetListView.as_view(), name='list'),
+    url(r'^create/$', TweetCreateView.as_view(), name='create'),
     url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'),
 ]
 
